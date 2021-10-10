4b587dafce85c20abb398fed377a4875f5a556f2
Timen Zandbergen <timen@zandbergen.dev>
2021-10-10 16:40:59 +0200

    Format lua files

--- neovim/.config/nvim/lua/tz/completion.lua
+++ neovim/.config/nvim/lua/tz/completion.lua
@@ -6,141 +6,128 @@ local WIDE_HEIGHT = 40
 local luasnip = require 'luasnip'
 
 -- require'lsp_signature'.setup()
-local lspkind = require'lspkind'
+local lspkind = require 'lspkind'
 lspkind.init({
-	-- enables text annotations
-	--
-	-- default: true
-	with_text = true,
+    -- enables text annotations
+    --
+    -- default: true
+    with_text = true,
 
-	-- default symbol map
-	-- can be either 'default' or
-	-- 'codicons' for codicon preset (requires vscode-codicons font installed)
-	--
-	-- default: 'default'
-	preset = 'default',
+    -- default symbol map
+    -- can be either 'default' or
+    -- 'codicons' for codicon preset (requires vscode-codicons font installed)
+    --
+    -- default: 'default'
+    preset = 'default',
 
-	-- override preset symbols
-	--
-	-- default: {}
-	symbol_map = {
-		Text = '',
-		Method = 'ƒ',
-		Function = '',
-		Constructor = '',
-		Variable = '',
-		Class = '',
-		Interface = 'ﰮ',
-		Module = '',
-		Property = '',
-		Unit = '',
-		Value = '',
-		Enum = '了',
-		Keyword = '',
-		Snippet = '﬌',
-		Color = '',
-		File = '',
-		Folder = '',
-		EnumMember = '',
-		Constant = '',
-		Struct = ''
-	},
+    -- override preset symbols
+    --
+    -- default: {}
+    symbol_map = {
+        Text = '',
+        Method = 'ƒ',
+        Function = '',
+        Constructor = '',
+        Variable = '',
+        Class = '',
+        Interface = 'ﰮ',
+        Module = '',
+        Property = '',
+        Unit = '',
+        Value = '',
+        Enum = '了',
+        Keyword = '',
+        Snippet = '﬌',
+        Color = '',
+        File = '',
+        Folder = '',
+        EnumMember = '',
+        Constant = '',
+        Struct = ''
+    }
 })
 cmp.setup {
-	completion = {
-		autocomplete = {
-			types.cmp.TriggerEvent.TextChanged,
-		},
-		completeopt = 'menu,menuone,noselect',
-		keyword_pattern = [[\%(-\?\d\+\%(\.\d\+\)\?\|\h\w*\%(-\w*\)*\)]],
-		keyword_length = 1,
-		get_trigger_characters = function(trigger_characters)
-			return trigger_characters
-		end
-	},
+    completion = {
+        autocomplete = {types.cmp.TriggerEvent.TextChanged},
+        completeopt = 'menu,menuone,noselect',
+        keyword_pattern = [[\%(-\?\d\+\%(\.\d\+\)\?\|\h\w*\%(-\w*\)*\)]],
+        keyword_length = 1,
+        get_trigger_characters = function(trigger_characters)
+            return trigger_characters
+        end
+    },
 
-	snippet = {
-		expand = function(args)
-			require('luasnip').lsp_expand(args.body)
-		end,
-	},
+    snippet = {
+        expand = function(args) require('luasnip').lsp_expand(args.body) end
+    },
 
-	preselect = types.cmp.PreselectMode.Item,
+    preselect = types.cmp.PreselectMode.Item,
 
-	documentation = {
-		border = { '', '', '', ' ', '', '', '', ' ' },
-		winhighlight = 'NormalFloat:CmpDocumentation,FloatBorder:CmpDocumentationBorder',
-		maxwidth = math.floor((WIDE_HEIGHT * 2) * (vim.o.columns / (WIDE_HEIGHT * 2 * 16 / 9))),
-		maxheight = math.floor(WIDE_HEIGHT * (WIDE_HEIGHT / vim.o.lines)),
-	},
+    documentation = {
+        border = {'', '', '', ' ', '', '', '', ' '},
+        winhighlight = 'NormalFloat:CmpDocumentation,FloatBorder:CmpDocumentationBorder',
+        maxwidth = math.floor((WIDE_HEIGHT * 2) *
+                                  (vim.o.columns / (WIDE_HEIGHT * 2 * 16 / 9))),
+        maxheight = math.floor(WIDE_HEIGHT * (WIDE_HEIGHT / vim.o.lines))
+    },
 
-	confirmation = {
-		default_behavior = types.cmp.ConfirmBehavior.Insert,
-		get_commit_characters = function(commit_characters)
-			return commit_characters
-		end
-	},
+    confirmation = {
+        default_behavior = types.cmp.ConfirmBehavior.Insert,
+        get_commit_characters = function(commit_characters)
+            return commit_characters
+        end
+    },
 
-	sorting = {
-		priority_weight = 2,
-		comparators = {
-			compare.offset,
-			compare.exact,
-			compare.score,
-			compare.kind,
-			compare.sort_text,
-			compare.length,
-			compare.order,
-		}
-	},
+    sorting = {
+        priority_weight = 2,
+        comparators = {
+            compare.offset, compare.exact, compare.score, compare.kind,
+            compare.sort_text, compare.length, compare.order
+        }
+    },
 
-	event = {},
+    event = {},
 
-	mapping = {
-		['<c-p>'] = cmp.mapping.complete(),
-		['<c-y>'] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}),
-		['<Tab>'] = function(fallback)
-			if vim.fn.pumvisible() == 1 then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
-			elseif luasnip.expand_or_jumpable() then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
-			else
-				fallback()
-			end
-		end,
-	},
+    mapping = {
+        ['<c-y>'] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true
+        }),
+        ['<Tab>'] = function(fallback)
+            if vim.fn.pumvisible() == 1 then
+                vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true,
+                                                               true, true), 'n')
+            elseif luasnip.expand_or_jumpable() then
+                vim.fn.feedkeys(vim.api.nvim_replace_termcodes(
+                                    '<Plug>luasnip-expand-or-jump', true, true,
+                                    true), '')
+            else
+                fallback()
+            end
+        end
+    },
 
-	formatting = {
-		deprecated = true,
-		format = function(entry, vim_item)
-			vim_item.kind = lspkind.presets.default[vim_item.kind] .. " " .. vim_item.kind
-			vim_item.menu = ({
-				buffer = "[Buffer]",
-				nvim_lsp = "[LSP]",
-				luasnip = "[LuaSnip]",
-				nvim_lua = "[Lua]",
-				latex_symbols = "[Latex]",
-				path = "[Path]",
-			})[entry.source.name]
-			return vim_item
-		end
-	},
+    formatting = {
+        deprecated = true,
+        format = function(entry, vim_item)
+            vim_item.kind = lspkind.presets.default[vim_item.kind] .. " " ..
+                                vim_item.kind
+            vim_item.menu = ({
+                buffer = "[Buffer]",
+                nvim_lsp = "[LSP]",
+                luasnip = "[LuaSnip]",
+                nvim_lua = "[Lua]",
+                latex_symbols = "[Latex]",
+                path = "[Path]"
+            })[entry.source.name]
+            return vim_item
+        end
+    },
 
-	experimental = {
-		ghost_text = false,
-	},
+    experimental = {ghost_text = false},
 
-	sources = {
-		{ name = 'luasnip' },
-		{ name = 'nvim_lsp' },
-		{ name = 'buffer' },
-		{ name = 'path' },
-		{ name = 'calc' },
-		{ name = 'spell' },
-	},
+    sources = {
+        {name = 'luasnip'}, {name = 'nvim_lsp'}, {name = 'buffer'},
+        {name = 'path'}, {name = 'calc'}, {name = 'spell'}
+    }
 }
-
-
--- neovim/.config/nvim/lua/tz/line.lua
+++ neovim/.config/nvim/lua/tz/line.lua
@@ -1,43 +1,42 @@
-
 local diagnostics = {
-	'diagnostics',
-	-- table of diagnostic sources, available sources:
-	-- nvim_lsp, coc, ale, vim_lsp
-	sources = {'nvim_lsp'},
-	-- displays diagnostics from defined severity
-	sections = {'error', 'warn', 'info', 'hint'},
-	-- all colors are in format #rrggbb
-	--color_error = nil, -- changes diagnostic's error foreground color
-	--color_warn = nil, -- changes diagnostic's warn foreground color
-	--color_info = nil, -- Changes diagnostic's info foreground color
-	--color_hint = nil, -- Changes diagnostic's hint foreground color
-	--symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'}
+    'diagnostics',
+    -- table of diagnostic sources, available sources:
+    -- nvim_lsp, coc, ale, vim_lsp
+    sources = {'nvim_lsp'},
+    -- displays diagnostics from defined severity
+    sections = {'error', 'warn', 'info', 'hint'}
+    -- all colors are in format #rrggbb
+    -- color_error = nil, -- changes diagnostic's error foreground color
+    -- color_warn = nil, -- changes diagnostic's warn foreground color
+    -- color_info = nil, -- Changes diagnostic's info foreground color
+    -- color_hint = nil, -- Changes diagnostic's hint foreground color
+    -- symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'}
 }
 
 require'lualine'.setup {
-	options = {
-		icons_enabled = true,
-		theme = 'material',
-		component_separators = {'', ''},
-		section_separators = {'', ''},
-		disabled_filetypes = {}
-	},
-	sections = {
-		lualine_a = {'mode'},
-		lualine_b = {'branch', 'diff'},
-		lualine_c = {'filename'},
-		lualine_x = {'filetype'},
-		lualine_y = {'progress', 'location'},
-		lualine_z = {diagnostics}
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = {'filename'},
-		lualine_x = {'location'},
-		lualine_y = {},
-		lualine_z = {diagnostics}
-	},
-	tabline = {},
-	extensions = {'quickfix'}
+    options = {
+        icons_enabled = true,
+        theme = 'material',
+        component_separators = {'', ''},
+        section_separators = {'', ''},
+        disabled_filetypes = {}
+    },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff'},
+        lualine_c = {'filename'},
+        lualine_x = {'filetype'},
+        lualine_y = {'progress', 'location'},
+        lualine_z = {diagnostics}
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {diagnostics}
+    },
+    tabline = {},
+    extensions = {'quickfix'}
 }

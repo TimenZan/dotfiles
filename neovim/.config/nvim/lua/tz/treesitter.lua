0813c19110c5ecaa627be344ca0de0760dab213d
Timen Zandbergen <timen@zandbergen.dev>
2021-12-06 12:39:17 +0100

    Enable syntax for conceal

--- neovim/.config/nvim/lua/tz/treesitter.lua
+++ neovim/.config/nvim/lua/tz/treesitter.lua
@@ -5,6 +5,7 @@ require'nvim-treesitter.configs'.setup {
     ensure_installed = "maintained",
     highlight = {
         enable = true,
+        additional_vim_regex_highlighting = true,
     },
     incremental_selection = {
         enable = true,

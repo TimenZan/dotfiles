f0c1112aee042196e401ef2b7c26a28e601d2fd9
Timen Zandbergen <timen@zandbergen.dev>
2021-12-10 15:15:07 +0100

    Update to modern diagnostics

--- neovim/.config/nvim/lua/tz/line.lua
+++ neovim/.config/nvim/lua/tz/line.lua
@@ -2,7 +2,7 @@ local diagnostics = {
     'diagnostics',
     -- table of diagnostic sources, available sources:
     -- nvim_lsp, coc, ale, vim_lsp
-    sources = {'nvim_lsp'},
+    sources = {'nvim_diagnostic'},
     -- displays diagnostics from defined severity
     sections = {'error', 'warn', 'info', 'hint'}
     -- all colors are in format #rrggbb

6cda52c4fcdf7d4ee252e1c6c827f84a40f7c8c7
Timen Zandbergen <timen@zandbergen.dev>
2022-02-11 13:46:24 +0100

    Disable lua treesitter grammar for now

--- neovim/.config/nvim/lua/tz/treesitter.lua
+++ neovim/.config/nvim/lua/tz/treesitter.lua
@@ -5,6 +5,7 @@ require'nvim-treesitter.configs'.setup {
     ensure_installed = "maintained",
     highlight = {
         enable = true,
+        disable = {"lua"},
         additional_vim_regex_highlighting = true,
     },
     incremental_selection = {

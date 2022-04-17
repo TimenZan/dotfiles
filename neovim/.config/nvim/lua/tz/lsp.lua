58ea45cb0f7ab455274703de9b8c29d1552f8349
Timen Zandbergen <timen@zandbergen.dev>
2022-04-17 17:49:00 +0200

    Add proselint lsp

--- neovim/.config/nvim/lua/tz/lsp.lua
+++ neovim/.config/nvim/lua/tz/lsp.lua
@@ -4,34 +4,54 @@ local servers = {
 }
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp
-                                                                     .protocol
-                                                                     .make_client_capabilities())
+.protocol
+.make_client_capabilities())
 local on_attach = function(client)
-    require'illuminate'.on_attach(client)
-    require'lsp_signature'.on_attach()
+    require 'illuminate'.on_attach(client)
+    require 'lsp_signature'.on_attach()
 end
 
 for _, server in pairs(servers) do
-    require'lspconfig'[server].setup {
+    require 'lspconfig'[server].setup {
         capabilities = capabilities,
         on_attach = on_attach
     }
 end
 
+local proselint = {
+    lintCommand = 'proselint -',
+    lintStdin = true,
+    lintFormats = { '%f:%l:%c: %m' },
+    lintSeverity = 3, --marks everything as Info severity, same as ltex
+}
 local efm_languages = {
-    lua = {{formatCommand = "lua-format -i", formatStdin = true}},
     vim = {
         {
             lintCommand = 'vint --enable-neovim --style-problems --no-color -',
             lintStdin = true,
-            lintFormats = '%f:%l:%c: %m'
+            lintFormats = { '%f:%l:%c: %m' }
         }
-    }
+    },
+    tex = {
+        proselint,
+    },
+    gitcommit = {
+        proselint,
+    },
+    markdown = {
+        proselint,
+    },
+    plaintext = {
+        proselint,
+    },
+    bib = {
+        proselint,
+    },
 }
 local efm_filetypes = {}
 for key, _ in pairs(efm_languages) do table.insert(efm_filetypes, key) end
 
-require'lspconfig'.efm.setup {
+require 'lspconfig'.efm.setup {
     on_attach = on_attach,
     capabilities = capabilities,
     init_options = {
@@ -41,11 +61,11 @@ require'lspconfig'.efm.setup {
         codeAction = true,
         completion = true
     },
-    settings = {rootMarkers = {'.git/'}, languages = efm_languages},
+    settings = { rootMarkers = { '.git/' }, languages = efm_languages },
     filetypes = efm_filetypes
 }
 
-require'lspconfig'.arduino_language_server.setup {
+require 'lspconfig'.arduino_language_server.setup {
     cmd = {
         "arduino-language-server",
         "-cli", "/bin/arduino-cli",
@@ -59,7 +79,7 @@ require'lspconfig'.arduino_language_server.setup {
 
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
-require'lspconfig'.sumneko_lua.setup {
+require 'lspconfig'.sumneko_lua.setup {
     capabilities = capabilities,
     cmd = {
         'lua-language-server', '-E', '/usr/share/lua-language-server/main.lua'
@@ -67,14 +87,14 @@ require'lspconfig'.sumneko_lua.setup {
     on_attach = on_attach,
     settings = {
         Lua = {
-            runtime = {version = 'LuaJIT', path = runtime_path},
-            diagnostics = {globals = {'vim'}},
+            runtime = { version = 'LuaJIT', path = runtime_path },
+            diagnostics = { globals = { 'vim' } },
             workspace = {
                 -- Make the server aware of Neovim runtime files
                 library = vim.api.nvim_get_runtime_file("", true)
             },
             -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {enable = false}
+            telemetry = { enable = false }
         }
     }
 }
@@ -97,37 +117,37 @@ local rust_opts = {
         hover_actions = {
             -- see vim.api.nvim_open_win()
             border = {
-                {"╭", "FloatBorder"}, {"─", "FloatBorder"},
-                {"╮", "FloatBorder"}, {"│", "FloatBorder"},
-                {"╯", "FloatBorder"}, {"─", "FloatBorder"},
-                {"╰", "FloatBorder"}, {"│", "FloatBorder"}
+                { "╭", "FloatBorder" }, { "─", "FloatBorder" },
+                { "╮", "FloatBorder" }, { "│", "FloatBorder" },
+                { "╯", "FloatBorder" }, { "─", "FloatBorder" },
+                { "╰", "FloatBorder" }, { "│", "FloatBorder" }
             }
         }
     },
     server = {
         capabilities = capabilities,
         on_attach = on_attach,
-        settings = {["rust-analyzer"] = {checkOnSave = {command = "clippy"}}}
+        settings = { ["rust-analyzer"] = { checkOnSave = { command = "clippy" } } }
     } -- options for rust-analyzer
 }
-require'rust-tools'.setup(rust_opts)
+require 'rust-tools'.setup(rust_opts)
 
 require("flutter-tools").setup({
-    widget_guides = {enabled = true},
-    closing_tags = {prefix = ">=> "},
-    dev_tools = {autostart = false, auto_open_browser = false},
+    widget_guides = { enabled = true },
+    closing_tags = { prefix = ">=> " },
+    dev_tools = { autostart = false, auto_open_browser = false },
     lsp = {
         capabilities = capabilities,
         on_attach = function(client)
-            require'illuminate'.on_attach(client)
+            require 'illuminate'.on_attach(client)
         end
     } -- options for dartls
 })
 
-require'nvim-autopairs'.setup()
+require 'nvim-autopairs'.setup()
 vim.cmd [[autocmd CursorMoved,CursorMovedI,InsertChange * lua require'nvim-lightbulb'.update_lightbulb()]]
 
-require'nvim-lightbulb'.setup {
+require 'nvim-lightbulb'.setup {
     sign = {
         enabled = true,
         -- Priority of the gutter sign
@@ -153,11 +173,11 @@ require'nvim-lightbulb'.setup {
         -- - winblend   transparency of the window (0-100)
         win_opts = {}
     },
-    virtual_text = {enabled = false, text = "💡"},
-    status_text = {enabled = false, text = "💡", text_unavailable = ""}
+    virtual_text = { enabled = false, text = "💡" },
+    status_text = { enabled = false, text = "💡", text_unavailable = "" }
 }
 
-require'trouble'.setup {
+require 'trouble'.setup {
     position = "bottom", -- position of the list can be: bottom, top, left, right
     height = 10, -- height of the trouble list when position is top or bottom
     width = 50, -- width of the list when position is left or right
@@ -170,18 +190,18 @@ require'trouble'.setup {
         close = "q", -- close the list
         cancel = "<esc>", -- cancel the preview and get back to your last window / buffer / cursor
         refresh = "r", -- manually refresh
-        jump = {"<cr>", "<tab>"}, -- jump to the diagnostic or open / close folds
-        open_split = {"<c-x>"}, -- open buffer in new split
-        open_vsplit = {"<c-v>"}, -- open buffer in new vsplit
-        open_tab = {"<c-t>"}, -- open buffer in new tab
-        jump_close = {"o"}, -- jump to the diagnostic and close the list
+        jump = { "<cr>", "<tab>" }, -- jump to the diagnostic or open / close folds
+        open_split = { "<c-x>" }, -- open buffer in new split
+        open_vsplit = { "<c-v>" }, -- open buffer in new vsplit
+        open_tab = { "<c-t>" }, -- open buffer in new tab
+        jump_close = { "o" }, -- jump to the diagnostic and close the list
         toggle_mode = "m", -- toggle between "workspace" and "document" diagnostics mode
         toggle_preview = "P", -- toggle auto_preview
         hover = "K", -- opens a small popup with the full multiline message
         preview = "p", -- preview the diagnostic location
-        close_folds = {"zM", "zm"}, -- close all folds
-        open_folds = {"zR", "zr"}, -- open all folds
-        toggle_fold = {"zA", "za"}, -- toggle fold of current file
+        close_folds = { "zM", "zm" }, -- close all folds
+        open_folds = { "zR", "zr" }, -- open all folds
+        toggle_fold = { "zA", "za" }, -- toggle fold of current file
         previous = "k", -- preview item
         next = "j" -- next item
     },

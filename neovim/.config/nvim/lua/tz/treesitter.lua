f1cb870f5541f9e24ea8b3114ab0aa4b5c8f3d23
Timen Zandbergen <timen@zandbergen.dev>
2022-05-11 13:00:55 +0200

    Update ts context config to current mode

--- neovim/.config/nvim/lua/tz/treesitter.lua
+++ neovim/.config/nvim/lua/tz/treesitter.lua
@@ -59,6 +59,6 @@ require 'nvim-treesitter.configs'.setup {
     }
 }
 
-require 'treesitter-context.config'.setup {
+require 'treesitter-context'.setup {
     enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
 }

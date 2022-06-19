fbc2cfd2fb8b6aa6ec4dcd39a74e73d3d8a21514
Timen Zandbergen <timen@zandbergen.dev>
2022-06-19 20:23:01 +0200

    Update to new dap ui config format

--- neovim/.config/nvim/lua/tz/dap.lua
+++ neovim/.config/nvim/lua/tz/dap.lua
@@ -8,24 +8,26 @@ require 'dapui'.setup {
         edit = "e",
         repl = "r",
     },
-    sidebar = {
-        elements = {
-            -- Provide as ID strings or tables with "id" and "size" keys
-            {
-                id = "scopes",
-                size = 0.25, -- Can be float or integer > 1
+    layout = {
+        {
+            elements = {
+                -- Provide as ID strings or tables with "id" and "size" keys
+                {
+                    id = "scopes",
+                    size = 0.25, -- Can be float or integer > 1
+                },
+                { id = "breakpoints", size = 0.25 },
+                { id = "stacks", size = 0.25 },
+                { id = "watches", size = 00.25 },
             },
-            { id = "breakpoints", size = 0.25 },
-            { id = "stacks", size = 0.25 },
-            { id = "watches", size = 00.25 },
+            size = 40,
+            position = "left", -- Can be "left", "right", "top", "bottom"
         },
-        size = 40,
-        position = "left", -- Can be "left", "right", "top", "bottom"
-    },
-    tray = {
-        elements = { "repl" },
-        size = 10,
-        position = "bottom", -- Can be "left", "right", "top", "bottom"
+        {
+            elements = { "repl", "console" },
+            size = 10,
+            position = "bottom", -- Can be "left", "right", "top", "bottom"
+        },
     },
     floating = {
         max_height = nil, -- These can be integers or a float between 0 and 1.

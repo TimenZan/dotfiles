514800e39b21225cd251a584baa6eb593b1f439c
Timen Zandbergen <timen@zandbergen.dev>
2023-02-05 17:22:10 +0100

    Experiment with only symbols on lsp pum

--- neovim/.config/nvim/lua/tz/completion.lua
+++ neovim/.config/nvim/lua/tz/completion.lua
@@ -10,39 +10,39 @@ require 'luasnip/loaders/from_vscode'.lazy_load()
 
 -- require'lsp_signature'.setup()
 local lspkind = require 'lspkind'
-lspkind.init({
-    -- enables text annotations
-    mode = 'symbol_text',
 
-    -- default symbol map
-    -- can be either 'default' or
-    -- 'codicons' for codicon preset (requires vscode-codicons font installed)
-    preset = 'default',
+-- lspkind.init({
+--     -- enables text annotations
+--     mode = 'symbol',
+--     -- default symbol map
+--     -- can be either 'default' or
+--     -- 'codicons' for codicon preset (requires vscode-codicons font installed)
+--     preset = 'default',
+--     -- override preset symbols
+--     symbol_map = {
+--         Text = '',
+--         Method = 'ƒ',
+--         Function = '',
+--         Constructor = '',
+--         Variable = '',
+--         Class = '',
+--         Interface = 'ﰮ',
+--         Module = '',
+--         Property = '',
+--         Unit = '',
+--         Value = '',
+--         Enum = '了',
+--         Keyword = '',
+--         Snippet = '﬌',
+--         Color = '',
+--         File = '',
+--         Folder = '',
+--         EnumMember = '',
+--         Constant = '',
+--         Struct = ''
+--     }
+-- })
 
-    -- override preset symbols
-    symbol_map = {
-        Text = '',
-        Method = 'ƒ',
-        Function = '',
-        Constructor = '',
-        Variable = '',
-        Class = '',
-        Interface = 'ﰮ',
-        Module = '',
-        Property = '',
-        Unit = '',
-        Value = '',
-        Enum = '了',
-        Keyword = '',
-        Snippet = '﬌',
-        Color = '',
-        File = '',
-        Folder = '',
-        EnumMember = '',
-        Constant = '',
-        Struct = ''
-    }
-})
 cmp.setup {
     completion = {
         autocomplete = { types.cmp.TriggerEvent.TextChanged },
@@ -110,19 +110,31 @@ cmp.setup {
 
     formatting = {
         deprecated = true,
-        format = function(entry, vim_item)
-            vim_item.kind = lspkind.presets.default[vim_item.kind] .. " " ..
-                vim_item.kind
-            vim_item.menu = ({
-                buffer = "[Buffer]",
-                nvim_lsp = "[LSP]",
-                luasnip = "[LuaSnip]",
-                nvim_lua = "[Lua]",
-                latex_symbols = "[Latex]",
-                path = "[Path]"
-            })[entry.source.name]
-            return vim_item
-        end
+        -- format = function(entry, vim_item)
+        --     vim_item.kind = lspkind.presets.default[vim_item.kind] .. " " ..
+        --         vim_item.kind
+        --     vim_item.menu = ({
+        --         buffer = "[Buffer]",
+        --         nvim_lsp = "[LSP]",
+        --         luasnip = "[LuaSnip]",
+        --         nvim_lua = "[Lua]",
+        --         latex_symbols = "[Latex]",
+        --         path = "[Path]"
+        --     })[entry.source.name]
+        --     return vim_item
+        -- end
+        format = lspkind.cmp_format({
+            mode = 'symbol', -- show only symbol annotations
+            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            before = function (entry, vim_item)
+                -- ...
+                return vim_item
+            end
+        })
     },
 
     experimental = { ghost_text = true },

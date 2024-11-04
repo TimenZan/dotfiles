4726096ffe4c211c4c865c7cf8d500412a1506eb
Timen Zandbergen <timen@zandbergen.dev>
2024-11-04 15:06:39 +0100

    Preliminary dap reconfig

--- neovim/.config/nvim/lua/plugins/debugger.lua
+++ neovim/.config/nvim/lua/plugins/debugger.lua
@@ -1,61 +1,74 @@
 local plugs = {}
 
-table.insert(plugs, {
-    'rcarriga/nvim-dap-ui',
-    name = "dapui",
-    opts = {
-        icons = { expanded = "", collapsed = "" },
-        mappings = {
-            expand = { "<CR>", "<2-LeftMouse>" },
-            open = "o",
-            remove = "d",
-            edit = "e",
-            repl = "r",
-        },
-        layout = {
-            {
-                elements = {
-                    -- Provide as ID strings or tables with "id" and "size" keys
-                    {
-                        id = "scopes",
-                        size = 0.25, -- Can be float or integer > 1
-                    },
-                    { id = "breakpoints", size = 0.25 },
-                    { id = "stacks",      size = 0.25 },
-                    { id = "watches",     size = 00.25 },
-                },
-                size = 40,
-                position = "left", -- Can be "left", "right", "top", "bottom"
-            },
-            {
-                elements = { "repl", "console" },
-                size = 10,
-                position = "bottom", -- Can be "left", "right", "top", "bottom"
-            },
-        },
-        floating = {
-            max_height = nil,  -- These can be integers or a float between 0 and 1.
-            max_width = nil,   -- Floats will be treated as percentage of your screen.
-            border = "single", -- Border style. Can be "single", "double" or "rounded"
-            mappings = {
-                close = { "q", "<Esc>" },
-            },
-        },
-        windows = { indent = 1 },
-    },
-    event = 'VeryLazy',
-})
+-- table.insert(plugs, {
+--     'rcarriga/nvim-dap-ui',
+--     dependencies = {
+--         'nvim-neotest/nvim-nio',
+--     },
+--     name = "dapui",
+--     opts = {
+--         icons = { expanded = "", collapsed = "" },
+--         mappings = {
+--             expand = { "<CR>", "<2-LeftMouse>" },
+--             open = "o",
+--             remove = "d",
+--             edit = "e",
+--             repl = "r",
+--         },
+--         layout = {
+--             {
+--                 elements = {
+--                     -- Provide as ID strings or tables with "id" and "size" keys
+--                     {
+--                         id = "scopes",
+--                         size = 0.25, -- Can be float or integer > 1
+--                     },
+--                     { id = "breakpoints", size = 0.25 },
+--                     { id = "stacks",      size = 0.25 },
+--                     { id = "watches",     size = 00.25 },
+--                 },
+--                 size = 40,
+--                 position = "left", -- Can be "left", "right", "top", "bottom"
+--             },
+--             {
+--                 elements = { "repl", "console" },
+--                 size = 10,
+--                 position = "bottom", -- Can be "left", "right", "top", "bottom"
+--             },
+--         },
+--         floating = {
+--             max_height = nil,  -- These can be integers or a float between 0 and 1.
+--             max_width = nil,   -- Floats will be treated as percentage of your screen.
+--             border = "single", -- Border style. Can be "single", "double" or "rounded"
+--             mappings = {
+--                 close = { "q", "<Esc>" },
+--             },
+--         },
+--         windows = { indent = 1 },
+--     },
+--     -- event = 'VeryLazy',
+--     lazy = true,
+-- })
 
 table.insert(plugs, {
     'mfussenegger/nvim-dap',
     dependencies = {
-        { 'theHamsta/nvim-dap-virtual-text', config = true, event = 'VeryLazy' },
-        'rcarriga/nvim-dap-ui',
+        'nvim-neotest/nvim-nio',
+        'theHamsta/nvim-dap-virtual-text',
+        { 'rcarriga/nvim-dap-ui', name = 'dapui' },
         'nvim-telescope/telescope-dap.nvim',
+        'LiadOz/nvim-dap-repl-highlights',
     },
 
-    config = function()
+    config = function ()
         local dap = require 'dap'
+        local ui = require 'dapui'
+
+        ui.setup {}
+        require 'nvim-dap-virtual-text'.setup {}
+        require 'nvim-dap-repl-highlights'.setup()
+        -- vim.cmd 'TSInstall dap_repl'
+        require 'nvim-treesitter.install'.ensure_installed 'dap_repl'
 
         dap.adapters.lldb = {
             type = 'executable',
@@ -68,7 +81,7 @@ table.insert(plugs, {
                 name = "Launch",
                 type = "lldb",
                 request = "launch",
-                program = function()
+                program = function ()
                     return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
                 end,
                 cwd = '${workspaceFolder}',
@@ -87,35 +100,65 @@ table.insert(plugs, {
                 runInTerminal = true,
             },
         }
+
+        dap.adapters.haskell = {
+            type = 'executable',
+            command = 'haskell-debug-adapter',
+            -- args = { '--hackage-version=0.0.33.0' },
+        }
+        dap.configurations.haskell = {
+            {
+                type = 'haskell',
+                request = 'launch',
+                name = 'Debug',
+                workspace = '${workspaceFolder}',
+                startup = "${file}",
+                stopOnEntry = true,
+                logFile = vim.fn.stdpath('data') .. '/haskell-dap.log',
+                logLevel = 'WARNING',
+                ghciEnv = vim.empty_dict(),
+                ghciPrompt = "λ: ",
+                -- Adjust the prompt to the prompt you see when you invoke the stack ghci command below
+                ghciInitialPrompt = "λ: ",
+                ghciCmd = "stack ghci --test --no-load --no-build --main-is TARGET --ghci-options -fprint-evld-with-show",
+            },
+        }
+
+        dap.listeners.before.attach.dapui_config = ui.open
+        dap.listeners.before.launch.dapui_config = ui.open
+        dap.listeners.before.event_terminated.dapui_config = ui.close
+        dap.listeners.before.event_exited.dapui_config = ui.close
     end,
 
-    event = 'VeryLazy',
+    -- event = 'VeryLazy',
 
-    cmd = {
-        'DapContinue',
-        'DapToggleBreakpoint',
-        'DapTerminate',
-        'DapStepOver',
-        'DapToggleRepl',
-        'DapStepOut',
-        'DapShowLog',
-        'DapLoadLaunchJSON',
-        'DapEval',
-        'DapStepInto',
-        'DapNew',
-        'DapSetLogLevel',
-        'DapRestartFrame',
-    },
+    -- cmd = {
+    --     'DapContinue',
+    --     'DapToggleBreakpoint',
+    --     'DapTerminate',
+    --     'DapStepOver',
+    --     'DapToggleRepl',
+    --     'DapStepOut',
+    --     'DapShowLog',
+    --     'DapLoadLaunchJSON',
+    --     'DapEval',
+    --     'DapStepInto',
+    --     'DapNew',
+    --     'DapSetLogLevel',
+    --     'DapRestartFrame',
+    -- },
 
     keys = {
-        { '<leader>nc',  function() require 'dap'.continue() end },
-        { '<leader>ns',  function() require 'dap'.step_over() end },
-        { '<leader>ni',  function() require 'dap'.step_into() end },
-        { '<leader>no',  function() require 'dap'.step_out() end },
-        { '<leader>nb',  function() require 'dap'.toggle_breakpoint() end },
-        { '<leader>nB',  function() require 'dap'.set_breakpoint() end },
-        { '<leader>nl',  function() require 'dap'.set_breakpoint() end },
-        { '<leader>nr',  function() require 'dap'.run_last() end },
+        { '<leader>nc',  function () require 'dap'.continue() end },
+        { '<leader>ns',  function () require 'dap'.step_over() end },
+        { '<leader>ni',  function () require 'dap'.step_into() end },
+        { '<leader>no',  function () require 'dap'.step_out() end },
+        { '<leader>nb',  function () require 'dap'.toggle_breakpoint() end },
+        { '<leader>nB',  function () require 'dap'.set_breakpoint() end },
+        { '<leader>nh',  function () require 'dap'.run_to_cursor() end },
+        { '<leader>nr',  function () require 'dap'.run_last() end },
+        { '<leader>n?',  function () require 'dapui'.eval(nil, { enter = true }) end },
+
         { '<leader>fnc', '<cmd>Telescope dap commands<cr>' },
         { '<leader>fns', '<cmd>Telescope dap configurations<cr>' },
         { '<leader>fnb', '<cmd>Telescope dap list_breakpoints<cr>' },

24797e42bef675c538ac500952cf2d552e7d12b5
Timen Zandbergen <timen@zandbergen.dev>
2024-11-04 16:18:20 +0100

    Update vimtex options

--- neovim/.config/nvim/lua/plugins/tex.lua
+++ neovim/.config/nvim/lua/plugins/tex.lua
@@ -3,12 +3,29 @@ local plugs = {}
 table.insert(plugs, {
     "lervag/vimtex",
     lazy = false,
-    ft = { 'tex', },
-    init = function()
-        vim.g.tex_flavor = 'lualatex'
-        vim.g.vimtex_compiler_progname = 'nvr'
-        vim.g.vimtex_quickfix_mode = 1
-        vim.g.vimtex_view_method = 'zathura'
+    init = function ()
+        vim.g.vimtex_quickfix_mode = 2
+        vim.g.vimtex_quickfix_open_on_warning = 0
+        vim.g.vimtex_quickfix_ignore_filters = {
+            'Underfull \\hbox',
+            'Overfull \\hbox',
+            'LaTeX Warning: .\\+ float specifier changed to',
+        }
+        -- vim.g.vimtex_imaps_enabled = 0
+        vim.g.vimtex_delim_toggle_mod_list = {
+            { '\\left', '\\right' },
+            { '\\big',  '\\big' },
+        }
+
+        if vim.fn.executable('zathura') then
+            vim.g.vimtex_view_method = 'zathura'
+        elseif vim.fn.executable('skim') then
+            vim.g.vimtex_view_method = 'skim'
+        end
+
+        vim.g.vimtex_compiler_latexmk = {
+            out_dir = 'latexmk_build',
+        }
     end,
 })
 

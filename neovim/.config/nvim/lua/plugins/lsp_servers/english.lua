b1695353879a6934612ba64495f6b3b2273d194c
= <timen@zandbergen.dev>
2024-12-07 14:26:09 +0100

    Add typos-lsp init

--- neovim/.config/nvim/lua/plugins/lsp_servers/english.lua
+++ neovim/.config/nvim/lua/plugins/lsp_servers/english.lua
@@ -3,4 +3,10 @@
 return {
     { harper_ls = {} },
     { ltex = require "tz.lsp.ltex" },
+    { typos_lsp = {
+        init_options = {
+            config = "~/.local/share/nvim/typos.toml",
+            diagnosticSeverity = "Warn",
+        }
+    }},
 }

772366fb380cdefa68971308bbc263dfbc8dc877
Timen Zandbergen <timen@zandbergen.dev>
2025-01-02 17:07:10 +0100

    Add git-messenger configuration

--- neovim/.config/nvim/lua/plugins/git.lua
+++ neovim/.config/nvim/lua/plugins/git.lua
@@ -6,10 +6,17 @@ local plugs = {
     'junegunn/gv.vim',
     { 'lewis6991/gitsigns.nvim', opts = {}, },
     'rhysd/conflict-marker.vim',
-    -- {'rhysd/git-messenger.vim'
-    --	let g:git_messenger_no_default_mappings=v:true
-    --	nmap <leader>gm <Plug>(git-messenger)
-    --	}
+    {
+        'rhysd/git-messenger.vim',
+        config = function ()
+            -- let g:git_messenger_no_default_mappings=v:true
+            vim.g.git_messenger_no_default_mappings = true
+            vim.g.git_messenger_extra_blame_args = '-MCw'
+            vim.g.git_messenger_floating_win_opts = { border = 'single' }
+            -- nmap <leader>gm <Plug>(git-messenger)
+            vim.keymap.set('n', '<leader>gm', '<Plug>(git-messenger)')
+        end
+    },
     {
         'rhysd/committia.vim',
         config = function ()

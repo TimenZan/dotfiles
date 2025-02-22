27b6626acab3bf81898c3b404c9b2f6f570f6125
Timen Zandbergen <timen@zandbergen.dev>
2025-02-22 18:02:22 +0100

    Axe ltex for its json crimes

--- neovim/.config/nvim/lua/plugins/lsp_servers/english.lua
+++ neovim/.config/nvim/lua/plugins/lsp_servers/english.lua
@@ -1,11 +1,12 @@
 -- Language servers for the English language
 
 return {
-    { ltex = require "tz.lsp.ltex" },
-    { typos_lsp = {
-        init_options = {
-            config = "~/.local/share/nvim/typos.toml",
-            diagnosticSeverity = "Warn",
-        }
-    }},
+    {
+        typos_lsp = {
+            init_options = {
+                config = "~/.local/share/nvim/typos.toml",
+                diagnosticSeverity = "Warn",
+            },
+        },
+    },
 }

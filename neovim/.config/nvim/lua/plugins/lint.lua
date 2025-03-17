09476357691caa9c082451f5a317e68ed62882d8
Timen Zandbergen <timen@zandbergen.dev>
2025-03-17 23:09:04 +0100

    Proper en_us linting

--- neovim/.config/nvim/lua/plugins/lint.lua
+++ neovim/.config/nvim/lua/plugins/lint.lua
@@ -6,18 +6,48 @@ table.insert(plugs, {
     'mfussenegger/nvim-lint',
     lazy = false,
     config = function ()
+        local lint = require 'lint'
+
+        local langtool = lint.linters.languagetool
+        -- vim.print(langtool.args)
+        -- require 'fidget'.notify(vim.inspect(langtool.args), "info")
+        -- langtool.args = {
+        --     "--autodetect", "--json",
+        --     "--config", "~/.config/LanguageTool/server.properties"
+        -- }
+
+        local nat_linters = { 'proselint', 'languagetool', }
+        -- local nat_linters = { 'proselint', }
+
         -- For all "english" filetypes, register an autocmd to run proselint
         for _, ft in ipairs(english) do
-            require 'lint'.linters_by_ft[ft] = { 'proselint', 'languagetool', }
+            for _, linter in ipairs(nat_linters) do
+                -- do not create virtual lines for spelling and grammar
+                local ns = lint.get_namespace(linter)
+                vim.diagnostic.config({ virtual_text = true, virtual_lines = false, }, ns)
+                -- make all suggestions hints
+                lint.linters[linter] =
+                    require("lint.util").wrap(lint.linters[linter], function (diagnostic)
+                        diagnostic.severity = vim.diagnostic.severity.HINT
+                        return diagnostic
+                    end)
+            end
+            lint.linters_by_ft[ft] = nat_linters
+
+
             vim.api.nvim_create_autocmd({ 'FileType', }, {
                 callback = function ()
-                    vim.api.nvim_create_autocmd({ 'BufWritePost', }, {
-                        callback = function () require 'lint'.try_lint() end,
+                    vim.api.nvim_create_autocmd({ 'FocusLost', }, {
+                        -- vim.api.nvim_create_autocmd({ 'BufWritePost', }, {
+                        callback = function ()
+                            require 'lint'.try_lint()
+                        end,
                     })
                 end,
                 pattern = ft,
             })
         end
+        lint.linters_by_ft.fish = { 'fish' }
     end,
 })
 

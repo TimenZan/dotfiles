03323c83e287d3af2e21384cbfc76bdc3d9e9235
Timen Zandbergen <timen@zandbergen.dev>
2025-03-21 12:24:20 +0100

    Remove clangd inlayhints as 0.10 satisfies it

--- neovim/.config/nvim/lua/plugins/lsp_servers/c.lua
+++ neovim/.config/nvim/lua/plugins/lsp_servers/c.lua
@@ -1,10 +1,5 @@
 return {
     {
-        clangd = {
-            on_attach = function(client)
-                require "clangd_extensions.inlay_hints".setup_autocmd()
-                require "clangd_extensions.inlay_hints".set_inlay_hints()
-            end
-        }
+        clangd = {}
     },
 }

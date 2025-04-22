828daf7fddd8726d4c3a2d2a06c2d11f6c757a54
Timen Zandbergen <timen@zandbergen.dev>
2025-04-22 09:44:27 +0200

    Don't check for updates every startup

--- neovim/.config/nvim/lua/config/lazy.lua
+++ neovim/.config/nvim/lua/config/lazy.lua
@@ -22,5 +22,5 @@ require("lazy").setup({
     -- colorscheme that will be used when installing plugins.
     install = { colorscheme = { "catppuccin" } },
     -- automatically check for plugin updates
-    checker = { enabled = true },
+    checker = { enabled = false },
 })

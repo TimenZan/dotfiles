3438bd9de5818b793a42ee50bb0b40b2c013d7ce
Timen Zandbergen <timen@zandbergen.dev>
2025-06-03 16:36:22 +0200

    Fix bibtex formatter problem

--- neovim/.config/nvim/lua/plugins/lsp_servers/latex.lua
+++ neovim/.config/nvim/lua/plugins/lsp_servers/latex.lua
@@ -1,3 +1,10 @@
 return {
-    { texlab = {} },
+    {
+        texlab = {
+            settings = {
+                -- disable formatter that breaks urls containing a tilde
+                texlab = { bibtexFormatter = "none", },
+            },
+        },
+    },
 }

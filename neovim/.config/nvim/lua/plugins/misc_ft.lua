054dcfa8e766f9e0c006bc7e4d128e4fc5a90770
Timen Zandbergen <timen@zandbergen.dev>
2025-06-12 18:41:41 +0200

    Add syntax for labeled bnf

--- neovim/.config/nvim/lua/plugins/misc_ft.lua
+++ neovim/.config/nvim/lua/plugins/misc_ft.lua
@@ -3,5 +3,11 @@ local plugs = {}
 table.insert(plugs, { 'cuducos/yaml.nvim' })
 table.insert(plugs, { 'HealsCodes/vim-gas' })
 table.insert(plugs, { 'aklt/plantuml-syntax' })
+table.insert(plugs, {
+    'vim-scripts/lbnf.vim',
+    config = function ()
+        vim.cmd [[ au bufreadpre,bufnewfile *.cf set ft=lbnf ]]
+    end
+})
 
 return plugs

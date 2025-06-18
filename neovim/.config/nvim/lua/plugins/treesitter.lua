6f045c66949e6f99744b252ba490fffd93e8a644
Timen Zandbergen <timen@zandbergen.dev>
2025-06-18 17:23:10 +0200

    Enable treesitter context by default

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -42,6 +42,7 @@ table.insert(plugs, {
         vim.keymap.set('n', '[c', function ()
             require 'treesitter-context'.go_to_context(vim.v.count1)
         end, { silent = true })
+        require 'treesitter-context'.setup {}
     end
 })
 

acbe4db1792a0d4b4739afb9fad41ded8768699e
Timen Zandbergen <timen@zandbergen.dev>
2026-01-23 15:55:55 +0100

    Add incoming calls binding

--- neovim/.config/nvim/lua/bindings.lua
+++ neovim/.config/nvim/lua/bindings.lua
@@ -27,6 +27,8 @@ set('n', '1gD', vim.lsp.buf.type_definition, { silent = true, })
 set('n', '<c-k>', vim.lsp.buf.signature_help, { silent = true, })
 set('n', '<leader>a', vim.lsp.buf.code_action, { silent = true, })
 set('n', '<leader>l', vim.lsp.codelens.run, { silent = true, })
+-- TODO: map to trouble.nvim?
+set('n', '<leader>i', vim.lsp.buf.incoming_calls, { silent = true, })
 set('n', '<leader>rn', vim.lsp.buf.rename, { silent = true, })
 set('n', 'g0', vim.lsp.buf.document_symbol, { silent = true, })
 set('n', 'g=', function () vim.lsp.buf.format { async = true } end, { silent = true, })
@@ -35,7 +37,7 @@ set('n', 'gW', vim.lsp.buf.workspace_symbol, { silent = true, })
 set('n', 'gd', vim.lsp.buf.declaration, { silent = true, })
 
 vim.keymap.set("n", "<Leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic error messages" })
-vim.keymap.set("n", "J", function() return "mz" .. vim.v.count .. "J`z" end, {
+vim.keymap.set("n", "J", function () return "mz" .. vim.v.count .. "J`z" end, {
     desc = "Join lines, keep cursor position",
     expr = true,
 })

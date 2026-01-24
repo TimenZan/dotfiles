fb2bb6ab50e5b360e221d618c83b1afa1ecb26aa
Timen Zandbergen <timen@zandbergen.dev>
2026-01-24 15:36:36 +0100

    Add toggle function for autoformat

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -27,7 +27,8 @@ table.insert(plugs, {
 
 table.insert(plugs, {
     'stevearc/conform.nvim',
-    opts = {
+    config = function ()
+        require 'conform'.setup {
         formatters_by_ft = {
             python = function (bufnr)
                 if require("conform").get_formatter_info("ruff_format", bufnr).available then
@@ -48,7 +49,27 @@ table.insert(plugs, {
             lsp_format = "fallback",
             timeout_ms = 500,
         },
-    },
+        }
+
+        -- yoinked from https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
+        vim.api.nvim_create_user_command("FormatDisable", function (args)
+            if args.bang then
+                -- FormatDisable! will disable formatting just for this buffer
+                vim.b.disable_autoformat = true
+            else
+                vim.g.disable_autoformat = true
+            end
+        end, {
+            desc = "Disable autoformat-on-save",
+            bang = true,
+        })
+        vim.api.nvim_create_user_command("FormatEnable", function ()
+            vim.b.disable_autoformat = false
+            vim.g.disable_autoformat = false
+        end, {
+            desc = "Re-enable autoformat-on-save",
+        })
+    end,
 })
 
 table.insert(plugs, {

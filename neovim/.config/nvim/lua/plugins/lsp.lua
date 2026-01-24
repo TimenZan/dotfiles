906e834c4389aba14a48e7817c679ed009180c3d
Timen Zandbergen <timen@zandbergen.dev>
2026-01-24 15:38:47 +0100

    Fix whitespace

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -29,26 +29,26 @@ table.insert(plugs, {
     'stevearc/conform.nvim',
     config = function ()
         require 'conform'.setup {
-        formatters_by_ft = {
-            python = function (bufnr)
-                if require("conform").get_formatter_info("ruff_format", bufnr).available then
-                    return { "ruff_format" }
-                else
-                    return { "isort", "black" }
-                end
-            end,
-            bibtex = { "bibtex-tidy" },
-            -- still experimental
-            latex = { "llf" },
-            fish = { "fish_indent" },
-        },
-        default_format_opts = {
-            lsp_format = "fallback",
-        },
-        format_on_save = {
-            lsp_format = "fallback",
-            timeout_ms = 500,
-        },
+            formatters_by_ft = {
+                python = function (bufnr)
+                    if require("conform").get_formatter_info("ruff_format", bufnr).available then
+                        return { "ruff_format" }
+                    else
+                        return { "isort", "black" }
+                    end
+                end,
+                bibtex = { "bibtex-tidy" },
+                -- still experimental
+                latex = { "llf" },
+                fish = { "fish_indent" },
+            },
+            default_format_opts = {
+                lsp_format = "fallback",
+            },
+            format_on_save = {
+                lsp_format = "fallback",
+                timeout_ms = 500,
+            },
         }
 
         -- yoinked from https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save

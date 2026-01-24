51e17dbe4151650845991a73d6624c1c1341cca8
Timen Zandbergen <timen@zandbergen.dev>
2026-01-24 03:06:43 +0100

    Update yamlls config

--- neovim/.config/nvim/lua/plugins/lsp_servers/data.lua
+++ neovim/.config/nvim/lua/plugins/lsp_servers/data.lua
@@ -1,6 +1,21 @@
 -- Language-servers for data files
 return {
-    { yamlls = { settings = { jaml = { schemaStore = { enable = false, url = "" }, schemas = require 'schemastore'.yaml.schemas(), }, }, }, },
+    {
+        yamlls = {
+            settings = {
+                yaml = {
+                    customTags = {
+                        '!reference sequence', -- necessary for gitlab-ci.yaml files
+                    },
+                    schemaStore = {
+                        enable = false,
+                        url = "",
+                    },
+                    schemas = require 'schemastore'.yaml.schemas(),
+                },
+            },
+        },
+    },
     { jsonls = { settings = { json = { schemas = require 'schemastore'.json.schemas(), validate = { enable = true }, }, }, }, },
     { taplo = {} }, -- Toml files
     { lemminx = { settings = { xml = { catalogs = { vim.fn.expand("$HOME/etc/xml/catalog") }, }, }, }, },
